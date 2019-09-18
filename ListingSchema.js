809c8679f6dce1b89f00da3a8b313d8d7e7d198f
marzkemark <marzkemark@ufl.edu>
2019-09-18 13:54:55 -0400

    Bootcamp2 updated with files

--- ListingSchema.js
+++ ListingSchema.js
@@ -7,8 +7,13 @@ var mongoose = require('mongoose'),
      See also https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
   */
 var listingSchema = new Schema({
-  /* Your code for a schema here */ 
-  //Check out - https://mongoosejs.com/docs/guide.html
+    code: {type: String, required: true}, 
+            name: {type: String, required: true}, 
+            coordinates: {
+                latitude: Number, 
+                longitude: Number
+            }, 
+            address: String
 
 });
 
@@ -16,7 +21,17 @@ var listingSchema = new Schema({
    See https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
 */
 listingSchema.pre('save', function(next) {
-  /* your code here */
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
--- queries.js
+++ queries.js
@@ -1,7 +1,11 @@
 /* Add all the required libraries*/
-
+var fs = require('fs'),
+    mongoose = require('mongoose'), 
+    Schema = mongoose.Schema, 
+    Listing = require('./ListingSchema.js'), 
+    config = require('./config');
 /* Connect to your database using mongoose - remember to keep your key secret*/
-
+mongoose.connect(config.db.uri, {useNewUriParser: true});
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
 
@@ -10,6 +14,10 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.findOne({'name': 'Library West'}, function(err, listing){
+    if (err) throw err;
+    console.log('Library West: ', listing);
+  })
 };
 var removeCable = function() {
   /*
@@ -17,6 +25,10 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  Listing.findOneAndRemove({'code': 'CABL'}, function(err, listing){
+    if (err) throw err;
+    console.log('Removed Cable: ', listing);
+  })
 };
 var updatePhelpsLab = function() {
   /*
@@ -26,14 +38,22 @@ var updatePhelpsLab = function() {
     Correct Address: 1953 Museum Rd, Gainesville, FL 32603
 
    */
+  Listing.findOneAndUpdate({'name': 'Phelps Laboratory'}, {'address': '1953 Museum Rd, Gainesville, FL 32603'}, {new: true}, function(err, listing){
+    if (err) throw err;
+    console.log('Updated Phelps Lab: ', listing);
+  })
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+  Listing.find(function(err, listings){
+    if (err) throw err;
+    console.log('All Listings: ', listings);
+  })
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
+updatePhelpsLab();
 retrieveAllListings();
